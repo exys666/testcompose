ec08c4c18ef3c35f506e94973b7efd551fdad1de
exys666 <exys666@gmail.com>
2018-08-01 22:12:39 +0200

    Prepare pom.xml to realease to Maven Central

--- src/main/kotlin/com/testcompose/annotation/Port.kt
+++ src/main/kotlin/com/testcompose/annotation/Port.kt
@@ -1,5 +1,10 @@
 package com.testcompose.annotation
 
+/**
+ * @property container id of container from docker-compose.yml
+ * @property port internal container port
+ * @property as system property under which port will be exposed
+ */
 @Target
 @Retention(AnnotationRetention.RUNTIME)
 annotation class Port (
