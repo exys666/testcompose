08748d694980b168a458c202cb39e05fc09f9528
yonatan <yonatan.wilkof@goflash.com>
2019-05-05 10:04:27 +0200

    improve message when cannot find assigned ports to a container

--- src/main/kotlin/com/testcompose/support/DockerCompose.kt
+++ src/main/kotlin/com/testcompose/support/DockerCompose.kt
@@ -18,7 +18,7 @@ class DockerCompose(
     }
 
     fun ps() =
-        DockerComposeCommand("-f", configPath, "-p", compositionId, "ps").waitForComplete()
+            DockerComposeCommand("-f", configPath, "-p", compositionId, "ps").waitForComplete()
 
 
     // TODO better name
@@ -26,10 +26,18 @@ class DockerCompose(
         DockerComposeCommand("-f", configPath, "-p", compositionId, "logs", "-f").waitForMessage(message)
     }
 
-    fun findPort(container: String, port: Int): Int =
-            DockerComposeCommand("-f", configPath, "-p", compositionId, "port", container, port.toString())
-                    .waitForComplete()[0]
-                    .substring("0.0.0.0:".length)
-                    .toInt()
-
+    fun findPort(container: String, port: Int): Int {
+        val assignedPort = DockerComposeCommand("-f", configPath, "-p", compositionId, "port", container, port.toString()).waitForComplete()
+        if (assignedPort.isEmpty()) {
+            throw IllegalStateException(
+                    "Could not find retrieve mapped port for container '$container' and port '$port'. This could happen due to:\n" +
+                            "1. container '$container' was not defined in compose config file.\n" +
+                            "2. container '$container' failed to start.\n" +
+                            "3. container '$container' does not expose port '$port'."
+            )
+        }
+        assignedPort[0]
+                .substring("0.0.0.0:".length)
+                .toInt()
+    }
 }
